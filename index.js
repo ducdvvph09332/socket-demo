1a00c2d32d3d568273a47e2883f0863bfd4ae813
Việt Đức <ducdvvph09332@fpt.edu.vn>
2020-12-30 14:07:40 +0700

    bin_0

--- index.js
+++ index.js
@@ -2,21 +2,44 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var axios = require('axios');
 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+});
+
 app.get('/sound', (req, res) => {
     res.sendFile(__dirname + '/audio/notify.mp3');
 });
 
 io.on('connection', (socket) => {
+    // Request API.
+    socket.on('check_login', data => {
+        axios
+        .post('http://localhost:1337/auth/local', {
+            identifier: `${data.email}`,
+            password: `${data.password}`,
+        })
+        .then(response => {
+            // Handle success.
+            var user = response.data.user
+            socket.emit('check_logged', user)
+        })
+        .catch(error => {
+            // Handle error.
+            socket.emit('check_logged', null) 
+        });
+    })
+
     let countUser = io.engine.clientsCount;
     socket.emit('user-connected', countUser);
     // join room
     socket.join(`${socket.id}`)
-        // console.log(socket.rooms);
     // get data
     socket.on('msg-sent', (data) => {
         // post data for all
@@ -24,7 +47,7 @@ io.on('connection', (socket) => {
             socket.broadcast.emit('msg', {
                 light: data.light,
                 temp: data.temp
-            }); 
+            });
         } else {
             // post data
             socket.to(`${data.notify_for}`).emit('msg', {
